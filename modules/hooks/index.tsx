3c8c07a158bc81489d2e8d219f0dd9964faf9628
X1Z53 <x1z53@yandex.ru>
2023-07-13 05:07:13 +0300

    v1.1.0.2
    Some little fixes

--- modules/hooks/index.tsx
+++ modules/hooks/index.tsx
@@ -1,6 +1,5 @@
 export { default as getDatabase } from "./getDatabase"
 export { default as getUrl } from "./getUrl"
 export { default as sortObjectArray } from "./sortObjectArray"
-export { default as useObjectState } from "./useObjectState"
 export { default as useToggle } from "./useToggle"
 
