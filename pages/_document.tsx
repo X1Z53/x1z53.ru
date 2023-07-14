8dc65470f5afb8ce6e709d424fefbe96e696e8d1
X1Z53 <x1z53@yandex.ru>
2023-07-15 02:47:03 +0300

    v1.2.1
    Added Yandex.Metrika

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { ColorModeScript, extendTheme } from "@chakra-ui/react"
+import { ColorModeScript, extendTheme, Image } from "@chakra-ui/react"
 import NextDocument, { Head, Html, Main, NextScript } from "next/document"
 
 const theme = extendTheme({ config: { initialColorMode: "dark" } })
@@ -14,6 +14,25 @@ export default class Document extends NextDocument {
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(94312353, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+                });
+              `
+            }} />
+          <noscript><Image src="https://mc.yandex.ru/watch/94312353" position="absolute" left="-9999px" alt="" /></noscript>
         </body>
       </Html>
     )
